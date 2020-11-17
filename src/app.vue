a38578f665f16db8a1a37334d51e9aaa8d90af8a
Salathiel Genese <salathielgenese@gmail.com>
2020-11-17 10:46:15 +0400

    feat: add a nav bar

--- src/app.vue
+++ src/app.vue
@@ -1,12 +1,16 @@
 <template>
-    <router-view/>
+    <Nav class="bg-gray-200"/>
+    <section class="p-4 pt-10">
+        <router-view/>
+    </section>
     <Footer class="px-4 py-8"/>
 </template>
 
 <script>
 import Footer from '@/app/shared/components/footer';
+import Nav from '@/app/shared/components/nav';
 export default {
     name: 'App',
-    components: { Footer },
+    components: { Nav, Footer },
 };
 </script>
