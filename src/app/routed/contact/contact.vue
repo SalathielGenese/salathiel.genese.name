0fba405daa446717f3d10d2f43641a854836867e
Salathiel Genese <salathielgenese@gmail.com>
2020-12-08 15:56:40 +0400

    feat: setup contact form with validation

--- src/app/routed/contact/contact.vue
+++ src/app/routed/contact/contact.vue
@@ -1,13 +1,75 @@
 <template>
-Contact / Available / Hire
+    <article>
+        <!--        <aside>-->
+        <!--            Contact Card-->
+        <!--        </aside>-->
+        <formfield>
+            <legend>
+                <h4 class="text-xl font-bold">
+                    Drop Me An Email
+                </h4>
+            </legend>
+
+            <form @submit.prevent="submit">
+                <label class="flex flex-col-reverse relative">
+                    <input type="text" v-model="form.email.$model"
+                           class="text-gray-600 focus:outline-none"/>
+                    <div v-if="form.email.$error"
+                         class="pointer-events-none invisible +focus:visible absolute top-full text-xs z-10 flex-col flex">
+                        <span style="clip-path: polygon(50% 13.34%, 100% 100%, 0 100%)"
+                              class="inline-block bg-red-600 h-2 w-2 origin-center rotate-90 ml-1"/>
+                        <span class="border-red-600 bg-gray-100 text-red-500 text-xs shadow border p-1">
+                            {{ form.email.$errors?.[ 0 ]?.$message }}
+                        </span>
+                    </div>
+                    <p class="font-bold text-sm">Email</p>
+                </label>
+                <label class="flex flex-col-reverse relative">
+                    <input type="text" v-model="form.lastname.$model"
+                           class="text-gray-600 focus:outline-none"/>
+                    <div v-if="form.lastname.$error"
+                         class="pointer-events-none invisible +focus:visible absolute top-full text-xs z-10 flex-col flex">
+                        <span style="clip-path: polygon(50% 13.34%, 100% 100%, 0 100%)"
+                              class="inline-block bg-red-600 h-2 w-2 origin-center rotate-90 ml-1"/>
+                        <span class="border-red-600 bg-gray-100 text-red-500 text-xs shadow border p-1">
+                            {{ form.lastname.$errors?.[ 0 ]?.$message }}
+                        </span>
+                    </div>
+                    <p class="font-bold text-sm">Lastname</p>
+                </label>
+                <label class="flex flex-col-reverse relative">
+                    <textarea v-model="form.content.$model"
+                              class="text-gray-600 focus:outline-none"/>
+                    <div v-if="form.content.$error"
+                         class="pointer-events-none invisible +focus:visible absolute top-full text-xs z-10 flex-col flex">
+                            <span style="clip-path: polygon(50% 13.34%, 100% 100%, 0 100%)"
+                                  class="inline-block bg-red-600 h-2 w-2 origin-center rotate-90 ml-1">
+                            </span>
+                        <span class="border-red-600 bg-gray-100 text-red-500 text-xs shadow border p-1">
+                                {{ form.content.$errors?.[ 0 ]?.$message }}
+                            </span>
+                    </div>
+                    <p class="font-bold text-sm">Content</p>
+                </label>
+
+                <footer>
+                    <button type="submit">
+                        Send
+                    </button>
+                </footer>
+            </form>
+        </formfield>
+    </article>
+    <!--    Contact / Available / Hire-->
 </template>
 
 <script>
+import { useContactForm } from '@/app/shared/composition/contact/use-contact.form';
+
 export default {
-  name: 'contact'
+    name: 'contact',
+    setup() {
+        return useContactForm();
+    },
 };
 </script>
-
-<style scoped>
-
-</style>
---
+++ src/app/shared/composition/contact/use-contact.form.js
@@ -0,0 +1,30 @@
+import { email, required } from '@vuelidate/validators';
+import useVuelidate from '@vuelidate/core';
+import { ref } from 'vue';
+
+export function useContactForm() {
+    const rules = {
+        email: { email, required },
+        lastname: { required },
+        content: { required },
+    };
+
+    const state = {
+        lastname: ref( '' ),
+        content: ref( '' ),
+        email: ref( '' ),
+    };
+
+    const form = useVuelidate( rules, state );
+
+    const submit = () => {
+        if ( !form.value.$invalid ) {
+            const value = Object.keys( state )
+                                .reduce( ( hay, key ) =>
+                                    ({ ...hay, [ key ]: state[ key ].value }), {} );
+            console.log( value );
+        }
+    };
+
+    return { form, state, submit };
+}
