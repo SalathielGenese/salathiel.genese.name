997f790f74fb17da29a99c5eeafd7e2d2c35dfda
Salathiel Genese <salathielgenese@gmail.com>
2020-12-08 15:56:10 +0400

    feat: configure @vuelidate

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import { router } from '@/app/routed';
 import { createApp } from 'vue';
 import './assets/tailwind.css';
 import App from './app.vue';
+import { VuelidatePlugin } from '@vuelidate/core';
 
 createApp( App )
     .use( router )
+    .use( VuelidatePlugin )
     .mount( 'main' );
