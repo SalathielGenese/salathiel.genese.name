15a9345cd8f7355d53ba36e9fc9a137efa7195a3
Salathiel Genese <salathielgenese@gmail.com>
2021-02-20 23:44:14 +0400

    feat: done designing dialog box component

--- src/app/routed/home/home.vue
+++ src/app/routed/home/home.vue
@@ -5,9 +5,10 @@
       <figure class="text-center">
         <div class="flex flex-row relative text-red-700 font-bold">
           <div class="flex-grow relative grid grid-rows-2">
-            <div class="text-right bottom-1/2 absolute w-full invisible md:visible">
-<!--              <a href="" class="inline-block px-2 py-1 mx-1">Background</a>-->
-<!--              <a href="" class="inline-block px-2 py-1 mx-1">Portfolio</a>-->
+            <!--            <div class="text-right bottom-1/2 absolute w-full invisible md:visible">-->
+            <div class="text-right bottom-1/2 absolute w-full">
+              <!--              <a href="" class="inline-block px-2 py-1 mx-1">Background</a>-->
+              <!--              <a href="" class="inline-block px-2 py-1 mx-1">Portfolio</a>-->
               <a href="https://docs.google.com/document/d/1lAMVLKyeDm2YrZdJl92gcg103YaSXBgjVDIeXBh3M1c"
                  target="_blank" class="inline-block px-2 py-1 mx-1">Resume</a>
             </div>
@@ -17,10 +18,12 @@
                  src="@/assets/img/profile/profile-picture.jpg">
           </picture>
           <div class="flex-grow relative grid grid-rows-2">
-            <div class="text-left bottom-1/2 absolute w-full invisible md:visible">
-<!--              <a href="" class="inline-block px-2 py-1 mx-1">Path</a>-->
-<!--              <a href="" class="inline-block px-2 py-1 mx-1">PodCast</a>-->
-              <a href="" class="inline-block px-2 py-1 mx-1">Contact</a>
+            <!--            <div class="text-left bottom-1/2 absolute w-full invisible md:visible">-->
+            <div class="text-left bottom-1/2 absolute w-full">
+              <!--              <a href="" class="inline-block px-2 py-1 mx-1">Path</a>-->
+              <!--              <a href="" class="inline-block px-2 py-1 mx-1">PodCast</a>-->
+              <a href="/contact" class="inline-block px-2 py-1 mx-1"
+                 @click.prevent="contactDialog.open()">Contact</a>
             </div>
           </div>
           <div class="h-1/2 w-full absolute bg-gray-800 bottom-0" style="z-index: -1"/>
@@ -37,20 +40,23 @@
     <div class="flex-grow border-none bg-gray-800"/>
 
 
-    <DialogBox title="Reach Out">
-      yyy
+    <DialogBox title="Contact" ref="contactDialog">
+      YYY
     </DialogBox>
   </section>
 </template>
 
-<script>
-import DialogBox from "@/app/shared/components/dialog-box";
-export default {
+<script lang="ts">
+import DialogBox from "@/app/shared/components/dialog-box.vue";
+import {defineComponent, ref} from "vue";
+
+export default defineComponent({
   name: 'Home',
-  components: {DialogBox}
-};
+  components: {DialogBox},
+  setup() {
+    return {
+      contactDialog: ref(),
+    };
+  },
+});
 </script>
-
-<style scoped>
-
-</style>
--- src/app/shared/components/dialog-box.vue
+++ src/app/shared/components/dialog-box.vue
@@ -1,40 +1,50 @@
 <template>
-  <!--  <teleport to="html > body > main">-->
-  <section class="flex top-0 left-0 flex-col w-screen min-h-screen absolute items-center"
-           style="background-color: rgba(0, 0, 0, .9)"
-           @click="onBackdropClick"
-           v-if="opened">
-    <hr class="flex-grow opacity-0"/>
-    <article class="px-4 py-2 z-10 m-6 md:m-10 bg-white rounded-sm inline-block">
-      <slot name="header">
-        <header>
-          <h2 class="py-1 text-2xl">
-            {{ title }}
-          </h2>
-          <hr>
-        </header>
-      </slot>
-      <slot/>
-    </article>
-    <hr class="flex-grow opacity-0"/>
-  </section>
-  <!--  </teleport>-->
+  <teleport to="html > body > main">
+    <section class="flex top-0 left-0 flex-col w-screen h-screen absolute items-center overflow-y-scroll"
+             style="background-color: rgba(0, 0, 0, .9)" @click="onBackdropClick" v-if="opened">
+      <hr class="flex-grow opacity-0"/>
+      <article class="px-4 py-2 z-10 m-6 md:m-10 bg-white rounded-sm inline-block" ref="articleRef">
+        <slot name="header">
+          <header>
+            <h2 class="py-1 text-2xl">{{ title }}</h2>
+            <hr>
+          </header>
+        </slot>
+        <slot/>
+      </article>
+      <hr class="flex-grow opacity-0"/>
+    </section>
+  </teleport>
 </template>
 
 <script lang="ts">
-export default {
+import {defineComponent, ref, watch} from "vue";
+
+export default defineComponent({
   name: 'DialogBox',
   props: {
     title: String,
+    onOpen: Function,
+    onClose: Function,
   },
-  setup(abc) {
+  setup(props) {
+    const opened = ref(false);
+    const articleRef = ref<HTMLElement>();
+
+    watch(opened, opened => opened ? props.onOpen?.() : props.onClose?.());
+
     return {
-      opened: true,
       onBackdropClick(event: Event) {
-        alert('Click');
-        console.log(event, abc)
+        if (!articleRef.value?.contains(event.target as any)) {
+          opened.value = false;
+        }
       },
+      open() {
+        opened.value = true;
+      },
+      articleRef,
+      opened,
     };
   },
-}
+})
 </script>
