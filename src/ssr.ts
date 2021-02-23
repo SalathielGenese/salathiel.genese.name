d14810a3abcabb8d1e1210681077b055174be243
Salathiel Genese <salathielgenese@gmail.com>
2021-02-23 15:34:48 +0400

    feat: adapt paths for production (see Dockerfile)

--- src/ssr.ts
+++ src/ssr.ts
@@ -4,18 +4,18 @@ import Express from 'express';
 import { promises as fs } from 'fs';
 import app from './main';
 
-const maybeWrap = fs.readFile( 'dist/index.html' )
+const maybeWrap = fs.readFile( 'index.html' )
     .then( buffer => buffer.toString() )
     .then( content => ( main: string ) =>
         content.replace( /<main(\s.*)>.*<\/main>/, `<main $1>${ main }</main>` ) );
 const { PORT = 8080 } = process.env;
 const server = Express();
 
-server.use( '/favicon.ico', Express.static( 'dist/favicon.ico' ) );
-server.use( '/fonts', Express.static( 'dist/fonts' ) );
-server.use( '/css', Express.static( 'dist/css' ) );
-server.use( '/img', Express.static( 'dist/img' ) );
-server.use( '/js', Express.static( 'dist/js' ) );
+server.use( '/favicon.ico', Express.static( 'favicon.ico' ) );
+server.use( '/fonts', Express.static( 'fonts' ) );
+server.use( '/css', Express.static( 'css' ) );
+server.use( '/img', Express.static( 'img' ) );
+server.use( '/js', Express.static( 'js' ) );
 server.get( '*', async ( request, response ) => {
     await router.push( request.url );
 
