2bbc94eb3e2af362b2d44598ac9b318fb2287ee9
Salathiel <salathiel@genese.name>
2023-12-04 22:05:35 +0100

    feat: layout nav and header for compatibility with Google Chrome and Firefox

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,7 +3,37 @@ import {Component} from "@angular/core";
 @Component({
     selector: 'section[path="/"]',
     template: `
-        Home
+        A-
+        <br>
+        B-
+        <br>
+        C-
+        <br>
+        D-
+        <br>
+        E-
+        <br>
+        F-
+        <br>
+        G-
+        <br>
+        H-
+        <br>
+        I-
+        <br>
+        J-
+        <br>
+        K-
+        <br>
+        L-
+        <br>
+        M-
+        <br>
+        N-
+        <br>
+        O-
+        <br>
+        P-
     `,
 })
 export class HomeComponent {
--- src/app/nav.component.ts
+++ src/app/nav.component.ts
@@ -1,9 +1,12 @@
 import {Component} from "@angular/core";
 
 @Component({
+    host: {'[class]': `'place-items-center backdrop-blur-sm h-[var(--nav-h)] justify-center bg-white/50 w-full left-0 fixed top-0 z-10 flex'`},
     selector: 'nav[sg-nav]',
     template: `
-        Nav
+        <span>A</span>
+        <hr class="flex-grow">
+        <span>Z</span>
     `,
 })
 export class NavComponent {
