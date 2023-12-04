af7948bdb92ed6bc5fcd694e708049a4731e92d0
Salathiel <salathiel@genese.name>
2023-12-04 20:17:38 +0100

    feat: scaffold footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-
-import {AppRoutingModule} from './app-routing.module';
-import {MainComponent} from './main.component';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import {NavComponent} from "./nav.component";
+
+import {HeaderComponent as HComponent} from "./components/header.component";
+import {NotFoundComponent} from "./error/not-found.component";
+import {AppRoutingModule} from './app-routing.module';
+import {HomeComponent} from "./home/home.component";
 import {HeaderComponent} from "./header.component";
 import {FooterComponent} from "./footer.component";
-import {NotFoundComponent} from "./error/not-found.component";
-import {HomeComponent} from "./home/home.component";
+import {MainComponent} from './main.component';
+import {NavComponent} from "./nav.component";
 
 @NgModule({
     declarations: [
+        HComponent,
+
         NavComponent,
         MainComponent,
         HeaderComponent,
--- src/app/footer.component.ts
+++ src/app/footer.component.ts
@@ -1,10 +1,59 @@
 import {Component} from "@angular/core";
+import {faBook} from "@fortawesome/free-solid-svg-icons/faBook";
+import {faEnvelope} from "@fortawesome/free-solid-svg-icons/faEnvelope";
+import {faPaperPlane} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
+    host: {'[class]': `'text-grey-400 min-h-[50vh] bg-grey-800 sm:grid-cols-2 grid-cols-1 gap-4 grid'`},
     selector: 'footer[sg-footer]',
     template: `
-        Footer
+        <div>
+            <h4 sgHeader>Credits</h4>
+            
+            <p>Made with &hearts; and:</p>
+            <ul class="list-disc ml-5">
+                <li>Git</li>
+                <li>GitHub</li>
+                <li>Angular</li>
+                <li>Firebase</li>
+                <li>TailwindCSS</li>
+                <li>Font Awesome</li>
+                <li>Google Cloud Platform Run</li>
+                <li>Google Cloud Platform Build</li>
+            </ul>
+        </div>
+        
+        <form>
+            <h4 sgHeader>Contact</h4>
+            
+            <label>
+                <span>EMAIL</span>
+                <small>How do I reach you out?</small>
+                <input type="email">
+                <del>req...</del>
+            </label>
+            <label>
+                <span>NAME</span>
+                <small>How should I call to you?</small>
+                <input type="text">
+                <del>Too shorts</del>
+            </label>
+            <label>
+                <span>MESSAGE</span>
+                <small>I'm all ears, sir/ma'am</small>
+                <textarea cols="10"></textarea>
+                <del>Requi...</del>
+            </label>
+            
+            <div>
+                <button type="submit">
+                    <fa-icon [icon]="faPaperPlane"></fa-icon>
+                    <span>send</span>
+                </button>
+            </div>
+        </form>
     `,
 })
 export class FooterComponent {
+    readonly faPaperPlane = faPaperPlane;
 }
