aa753bccbed9e07605e67887a2be17b3e5dd062f
Salathiel <salathiel@genese.name>
2024-01-10 23:19:12 +0100

    feat: rename a resource

--- terraform/that.tf
+++ terraform/that.tf
@@ -1,4 +1,4 @@
-resource "null_resource" "this" {
+resource "null_resource" "that" {
   triggers = {that-uuid= uuid()}
   provisioner "local-exec" {
     command = "echo this-uuid = ${self.triggers.that-uuid}"
