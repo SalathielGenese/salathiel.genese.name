e36ddac71ac9acfa3ff0243983b27966d4053a1b
Salathiel <salathiel@genese.name>
2024-01-10 18:11:34 +0100

    fix: try with another resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,6 +11,13 @@ locals {
   tfc-random-uuid = uuid()
 }
 
+resource "null_resource" "this" {
+  triggers = {this-uuid= uuid()}
+  provisioner "local-exec" {
+    command = "echo this-uuid = ${self.triggers.this-uuid}"
+  }
+}
+
 resource "terraform_data" "echo" {
   triggers_replace = local.tfc-random-uuid
   provisioner "local-exec" {
