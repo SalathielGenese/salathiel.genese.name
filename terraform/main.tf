9d072695bbc3875613e4f7aaba5c367c63788079
Salathiel <salathiel@genese.name>
2024-01-11 19:02:49 +0100

    fix: state GitHub Connection secret manager

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 data "google_project" "this" {}
 
 data "google_secret_manager_secret" "github-connection" {
-  secret_id = "projects/${data.google_project.this.number}/secrets/${var.google-cloud-platform-github-connection-secret-name}"
+  secret_id = var.google-cloud-platform-github-connection-secret-name
 }
