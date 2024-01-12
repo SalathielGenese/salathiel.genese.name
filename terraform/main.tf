785c3a4ccf88cd06c22c5c1b89e9ddd4f8cba1f1
Salathiel <salathiel@genese.name>
2024-01-12 03:56:41 +0100

    fix: terraform crash

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,35 +1,16 @@
 data "google_project" "this" {}
 
-import {
-  id = "projects/${data.google_project.this.number}/secrets/${var.google-cloud-platform-github-connection-secret-name}"
-  to = google_secret_manager_secret.github-connection
-}
-resource "google_secret_manager_secret" "github-connection" {
-  #  secret_id = var.google-cloud-platform-github-connection-secret-name
-  secret_id = ""
-  replication {
-    auto {}
-  }
-}
-#data "google_secret_manager_secret" "github-connection" {
-#  secret_id = var.google-cloud-platform-github-connection-secret-name
-#}
-import {
-  id = "projects/${data.google_project.this.number}/secrets/${var.google-cloud-platform-github-connection-secret-name}/versions/lates"
-  to = google_secret_manager_secret_version.github-connection
-}
-resource "google_secret_manager_secret_version" "github-connection" {
-  secret      = google_secret_manager_secret.github-connection.id
-  secret_data = ""
+data "google_secret_manager_secret" "github-connection" {
+  secret_id = var.google-cloud-platform-github-connection-secret-name
 }
 
-#data "google_secret_manager_secret_version" "github-connection" {
-#  secret = data.google_secret_manager_secret.github-connection.id
-#}
+data "google_secret_manager_secret_version" "github-connection" {
+  secret = data.google_secret_manager_secret.github-connection.id
+}
 
 import {
   #  id = "projects/${local.project}/locations/${local.region}/connections/${local.project}"
-  id = "projects/${local.project}/locations/${local.region}/connections/${local.project}"
+  id = "projects/${data.google_project.this.number}/locations/${local.region}/connections/${local.project}"
   to = google_cloudbuildv2_connection.github-connection
 }
 resource "google_cloudbuildv2_connection" "github-connection" {
