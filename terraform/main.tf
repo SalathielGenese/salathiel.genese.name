c63daf9ac7f70314e85755dbcd380d5598550093
Salathiel <salathiel@genese.name>
2024-01-12 01:47:28 +0100

    feat: support location for GitHub OAuth token GCP Secret Manager secret

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,4 +2,5 @@ data "google_project" "this" {}
 
 data "google_secret_manager_secret" "github-connection" {
   secret_id = var.google-cloud-platform-github-connection-secret-name
+  location = local.region
 }
