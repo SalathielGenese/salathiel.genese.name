c84ed5517b90c4e7c7974b6e74f0f9e503bd67d2
Salathiel <salathiel@genese.name>
2024-01-12 03:54:08 +0100

    feat: import existing connection resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,47 @@
 data "google_project" "this" {}
 
-data "google_secret_manager_secret" "github-connection" {
-  secret_id = var.google-cloud-platform-github-connection-secret-name
+import {
+  id = "projects/${data.google_project.this.number}/secrets/${var.google-cloud-platform-github-connection-secret-name}"
+  to = google_secret_manager_secret.github-connection
+}
+resource "google_secret_manager_secret" "github-connection" {
+  #  secret_id = var.google-cloud-platform-github-connection-secret-name
+  secret_id = ""
+  replication {
+    auto {}
+  }
+}
+#data "google_secret_manager_secret" "github-connection" {
+#  secret_id = var.google-cloud-platform-github-connection-secret-name
+#}
+import {
+  id = "projects/${data.google_project.this.number}/secrets/${var.google-cloud-platform-github-connection-secret-name}/versions/lates"
+  to = google_secret_manager_secret_version.github-connection
+}
+resource "google_secret_manager_secret_version" "github-connection" {
+  secret      = google_secret_manager_secret.github-connection.id
+  secret_data = ""
 }
 
-data "google_secret_manager_secret_version" "github-connection" {
-  secret = data.google_secret_manager_secret.github-connection.id
+#data "google_secret_manager_secret_version" "github-connection" {
+#  secret = data.google_secret_manager_secret.github-connection.id
+#}
+
+import {
+  #  id = "projects/${local.project}/locations/${local.region}/connections/${local.project}"
+  id = "projects/${local.project}/locations/${local.region}/connections/${local.project}"
+  to = google_cloudbuildv2_connection.github-connection
+}
+resource "google_cloudbuildv2_connection" "github-connection" {
+  #  name = local.project
+  #  location = local.region
+  #
+  #  github_config {
+  #    app_installation_id = var.google-cloud-platform-github-connection-app-install-id
+  #    authorizer_credential {
+  #      oauth_token_secret_version = data.google_secret_manager_secret_version.github-connection.id
+  #    }
+  #  }
+  location = ""
+  name     = ""
 }
