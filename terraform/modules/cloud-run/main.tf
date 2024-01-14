3703dee85a2e06ceb2226cba154fdeaa2ddbb9c4
Salathiel <salathiel@genese.name>
2024-01-14 09:20:13 +0100

    fix: disable domain mapping pending domain ownership verification

--- terraform/modules/cloud-run/main.tf
+++ terraform/modules/cloud-run/main.tf
@@ -16,25 +16,25 @@ resource "google_cloud_run_service_iam_binding" "this" {
   members = ["allUsers"]
 }
 
-resource "google_cloud_run_domain_mapping" "this-www" {
-  count    = var.domain-www-subdomain ? 1 : 0
-  name     = "www.${var.domain}"
-  location = var.region
-  spec {
-    route_name = google_cloud_run_v2_service.this.name
-  }
-  metadata {
-    namespace = var.project-number
-  }
-}
-
-resource "google_cloud_run_domain_mapping" "this" {
-  name     = "salathiel.genese.name"
-  location = var.region
-  spec {
-    route_name = google_cloud_run_v2_service.this.name
-  }
-  metadata {
-    namespace = var.project-number
-  }
-}
+#resource "google_cloud_run_domain_mapping" "this-www" {
+#  count    = var.domain-www-subdomain ? 1 : 0
+#  name     = "www.${var.domain}"
+#  location = var.region
+#  spec {
+#    route_name = google_cloud_run_v2_service.this.name
+#  }
+#  metadata {
+#    namespace = var.project-number
+#  }
+#}
+#
+#resource "google_cloud_run_domain_mapping" "this" {
+#  name     = "salathiel.genese.name"
+#  location = var.region
+#  spec {
+#    route_name = google_cloud_run_v2_service.this.name
+#  }
+#  metadata {
+#    namespace = var.project-number
+#  }
+#}
