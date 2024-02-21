daf84b7aba9f08bf950c0dc8c43bcf9646089916
Salathiel <salathiel@genese.name>
2024-02-21 10:14:32 +0100

    fix: language title should be in that language

--- src/constant.ts
+++ src/constant.ts
@@ -1,6 +1,6 @@
 export const LANGUAGES = [
   { tag: 'en-GB', title: 'English (Great Britain)' },
-  { tag: 'fr-FR', title: 'French (France)' },
+  { tag: 'fr-FR', title: 'Français (France)' },
 ];
 export const COOKIE_LANGUAGE_TAG = 'x-language-tag';
 export const ACCEPT_LANGUAGE_HEADER = 'accept-language';
