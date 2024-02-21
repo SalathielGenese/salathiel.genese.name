437fb5da828312fef0088d50211367a409db1d70
Salathiel <salathiel@genese.name>
2024-02-21 04:54:25 +0100

    feat: add language switch in the website footer

--- src/app/footer.component.ts
+++ src/app/footer.component.ts
@@ -1,10 +1,14 @@
-import {Component} from "@angular/core";
+import {Component, Inject, PLATFORM_ID, Signal} from "@angular/core";
 import {faLinkedinIn} from "@fortawesome/free-brands-svg-icons/faLinkedinIn";
 import {faFacebook} from "@fortawesome/free-brands-svg-icons/faFacebook";
 import {faTwitter} from "@fortawesome/free-brands-svg-icons/faTwitter";
 import {faGithub} from "@fortawesome/free-brands-svg-icons/faGithub";
 import {faTiktok} from "@fortawesome/free-brands-svg-icons/faTiktok";
 import {IconDefinition} from "@fortawesome/free-solid-svg-icons";
+import {COOKIE_LANGUAGE_TAG, LANGUAGES} from "../constant";
+import {LANGUAGE_TAG} from "./token";
+import {isPlatformBrowser} from "@angular/common";
+import {Router} from "@angular/router";
 
 @Component({
   host: {'[class]': `'justify-items-center place-items-center snap-always snap-center bg-grey-800 min-h-[50vh] grid p-8'`},
@@ -24,6 +28,19 @@ import {IconDefinition} from "@fortawesome/free-solid-svg-icons";
                   </a>
               </li>
           </ul>
+
+          <div class="text-center">
+              <select class="border-grey-700/50 bg-transparent text-grey-400 border p-1"
+                      (change)="setLanguageTag(languageTagRef.value)"
+                      #languageTagRef>
+                  <option [selected]="language.tag === languageTag()"
+                          *ngFor="let language of languages"
+                          class="bg-grey-700"
+                          [value]="language.tag">
+                      {{ language.title }}
+                  </option>
+              </select>
+          </div>
       </div>
   `,
 })
@@ -35,4 +52,21 @@ export class FooterComponent {
     {icon: faTiktok, uri: 'https://www.tiktok.com/@salathielgenese'},
     {icon: faFacebook, uri: 'https://www.facebook.com/SalathielGenese'},
   ];
+  protected readonly languages = LANGUAGES;
+
+  constructor(private readonly router: Router,
+              @Inject(PLATFORM_ID) private readonly platformId: object,
+              @Inject(LANGUAGE_TAG) protected readonly languageTag: Signal<string>) {
+  }
+
+  protected setLanguageTag(languageTag: string) {
+    if (isPlatformBrowser(this.platformId)) {
+      const next = `/${languageTag}${this.router.url.substring(1 + this.languageTag().length)}`;
+      this.router.navigateByUrl(next, {onSameUrlNavigation: 'reload'})
+          .then(() => {
+            document.cookie = `${COOKIE_LANGUAGE_TAG}=${languageTag};SameSite=Strict;Max-Age=${365 * 86_400};`;
+          })
+          .catch(console.error);
+    }
+  }
 }
