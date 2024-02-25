e7800b4ba8cd6c458884c259c76a2a406098a5ae
Salathiel <salathiel@genese.name>
2024-02-25 22:39:09 +0100

    feat: translate navigation bar

--- src/app/nav.component.ts
+++ src/app/nav.component.ts
@@ -2,10 +2,10 @@ import {Component, Inject, Signal} from "@angular/core";
 import {LANGUAGE_TAG} from "./token";
 
 @Component({
-  host: {'[class]': `'backdrop-blur lowercase w-full fixed py-4 z-10 flex'`},
+  host: {'[class]': `'backdrop-blur w-full fixed py-4 z-10 flex'`},
   selector: 'nav[sg-nav]',
   template: `
-      <a class="text-grey-400 bg-grey-100 origin-center -rotate-45 text-center normal-case -left-16 fixed w-48"
+      <a class="text-grey-400 bg-grey-100 origin-center -rotate-45 text-center -left-16 fixed w-48"
          href="https://github.com/SalathielGenese/salathiel.genese.name"
          target="_blank">
           <small>GitHub</small>
@@ -15,14 +15,14 @@ import {LANGUAGE_TAG} from "./token";
          translate="pages.home.title"
       ></a>
       <hr class="pointer-events-none opacity-0 flex-grow">
-      <a [routerLink]="[languageTag(), 'blog']"
-         class="sm:px-8 sm:py-4 px-4 py-2">
-          Blog
-      </a>
-      <a class="border-white text-white bg-brown rounded border sm:my-3 shadow px-4 py-0.5 my-1.5"
-         [routerLink]="[languageTag(), 'hire']">
-          Hire
-      </a>
+      <a class="lowercase sm:px-8 sm:py-4 px-4 py-2"
+         [routerLink]="[languageTag(), 'blog']"
+         translate="pages.blog.title"
+      ></a>
+      <a class="border-white text-white lowercase bg-brown rounded border sm:my-3 shadow px-4 py-0.5 my-1.5"
+         [routerLink]="[languageTag(), 'hire']"
+         translate="pages.hire.title"
+      ></a>
   `,
 })
 export class NavComponent {
