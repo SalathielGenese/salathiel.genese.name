27dc3c18abbf48cba6d085e01e02779ce65c08ec
Salathiel <salathiel@genese.name>
2024-02-25 23:16:04 +0100

    feat: define routes' title

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,9 @@ import {BlogComponent} from "./pages/blog.component";
 import {HireComponent} from "./pages/hire.component";
 
 const routes: Routes = [
-  {path: ':languageTag', pathMatch: 'full', component: HomeComponent},
-  {path: ':languageTag/blog', component: BlogComponent},
-  {path: ':languageTag/hire', component: HireComponent},
+  {path: ':languageTag', pathMatch: 'full', component: HomeComponent, title: 'pages.home.title'},
+  {path: ':languageTag/blog', component: BlogComponent, title: 'pages.blog.title'},
+  {path: ':languageTag/hire', component: HireComponent, title: 'pages.hire.title'},
   {path: '**', component: NotFoundComponent},
 ];
 
