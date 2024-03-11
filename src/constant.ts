8234a2acdb8b5c94806871d434de0f9d1bfa5ee3
Salathiel <salathiel@genese.name>
2024-03-11 01:48:38 +0100

    feat: refactor DIST_FOLDER for ease of re-use

--- src/constant.ts
+++ src/constant.ts
@@ -2,6 +2,5 @@ export const LANGUAGES = [
   {tag: 'en-GB', title: 'English (Great Britain)'},
   {tag: 'fr-FR', title: 'Français (France)'},
 ];
-export const KEY_DIST_FOLDER = 'DIST_FOLDER';
 export const COOKIE_LANGUAGE_TAG = 'x-language-tag';
 export const ACCEPT_LANGUAGE_HEADER = 'accept-language';
