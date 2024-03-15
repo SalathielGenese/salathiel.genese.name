7dd3166c7bc48f7f05bb68461138525cec13a425
Salathiel <salathiel@genese.name>
2024-03-15 01:21:13 +0100

    feat: break dependency between IS_HOME, LANGUAGE_TAG and AppModule, but also fallback to cookie when the URL prefix does not match supported language tag, as would be the case for fallback route - "**"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import {computed, effect, inject, InjectionToken, NgModule, PLATFORM_ID, Signal, signal} from '@angular/core';
+import {ActivatedRoute, NavigationEnd, Router, RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./pages/home.component";
 import {NotFoundComponent} from "./pages/not-found.component";
 import {BlogComponent} from "./pages/blog.component";
 import {HireComponent} from "./pages/hire.component";
+import {IS_HOME, LANGUAGE_TAG} from "./token";
+import {isPlatformBrowser} from "@angular/common";
+import {filter} from "rxjs";
+import {COOKIE_LANGUAGE_TAG, LANGUAGES} from "../constant";
+import {REQUEST} from "@nguniversal/express-engine/tokens";
+import {toSignal} from "@angular/core/rxjs-interop";
 
 const routes: Routes = [
   {path: ':languageTag', pathMatch: 'full', component: HomeComponent, title: 'pages.home.title'},
@@ -12,11 +18,67 @@ const routes: Routes = [
   {path: '**', component: NotFoundComponent},
 ];
 
+const NAVIGATION_END = new InjectionToken<Signal<NavigationEnd>>('NAVIGATION_END');
+
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    initialNavigation: 'enabledBlocking',
-  })],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      initialNavigation: 'enabledBlocking',
+    }),
+  ],
+  providers: [
+    {
+      useFactory: () => toSignal(inject(Router).events.pipe(filter(_ => _ instanceof NavigationEnd))),
+      provide: NAVIGATION_END,
+    },
+    {
+      useFactory: () => {
+        const activatedRoute = inject(ActivatedRoute);
+        const navigationEnd = inject(NAVIGATION_END);
+        return computed(() => {
+          navigationEnd();
+          return HomeComponent === activatedRoute.children[0]?.component;
+        })
+      },
+      provide: IS_HOME,
+    },
+    {
+      provide: LANGUAGE_TAG,
+      useFactory: () => {
+        const platformId = inject(PLATFORM_ID);
+        const navigationEnd = inject(NAVIGATION_END);
+        const request = inject(REQUEST, {optional: true});
+        const languageTag = signal((url => LANGUAGES.find(({tag}) => url.startsWith(`/${tag}`))?.tag!)(resolveUrl()));
+        effect(() => {
+          languageTag.set((url => LANGUAGES.find(({tag}) => url.startsWith(`/${tag}`))?.tag)(resolveUrl())
+              ?? resolveCookieLanguageTag()
+              ?? 'en-GB');
+          navigationEnd();
+        }, {allowSignalWrites: true});
+        return languageTag;
+
+        function resolveCookieLanguageTag() {
+          if (isPlatformBrowser(platformId)) {
+            const {cookie} = document;
+            return LANGUAGES.find(({tag}) => cookie.includes(`${COOKIE_LANGUAGE_TAG}=${tag}`))?.tag;
+          } else {
+            return request?.cookies?.[COOKIE_LANGUAGE_TAG];
+          }
+        }
+
+        function resolveUrl() {
+          if (isPlatformBrowser(platformId)) {
+            return location.pathname;
+          } else {
+            return request?.url!;
+          }
+        }
+      },
+    }
+  ],
+  exports: [
+    RouterModule,
+  ],
 })
 export class AppRoutingModule {
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,46 +1,30 @@
-import {
-  effect,
-  Inject,
-  inject,
-  InjectionToken,
-  NgModule,
-  Optional,
-  PLATFORM_ID,
-  signal,
-  Signal,
-  WritableSignal
-} from '@angular/core';
-import {toSignal} from "@angular/core/rxjs-interop";
 import {BrowserModule, provideClientHydration} from '@angular/platform-browser';
-import {ActivatedRoute, NavigationEnd, Router, TitleStrategy} from "@angular/router";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {ReactiveFormsModule} from "@angular/forms";
+import {TitleStrategy} from "@angular/router";
+import {NgModule} from '@angular/core';
+
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
-import {filter} from "rxjs";
-
-import {IS_HOME, LANGUAGE_TAG} from "./token";
+import {AppRoutingModule} from './app-routing.module';
 
 import {HeaderComponent as HComponent} from "./components/header.component";
-import {AppRoutingModule} from './app-routing.module';
-import {HeaderComponent} from "./header.component";
-import {FooterComponent} from "./footer.component";
-import {MainComponent} from './main.component';
-import {NavComponent} from "./nav.component";
+import {TranslateComponent} from "./components/translate.component";
+
+import {SalathielTitleStrategy} from "./services/salathiel.title-strategy";
+import {TargetInterceptor} from "./services/target.interceptor";
+import {I18nService} from "./services/i18n.service";
 
 import {NotFoundComponent} from "./pages/not-found.component";
 import {BlogComponent} from "./pages/blog.component";
 import {HireComponent} from "./pages/hire.component";
 import {HomeComponent} from "./pages/home.component";
-import {I18nService} from "./services/i18n.service";
 import {TranslatePipe} from "./pipes/translate.pipe";
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import {TargetInterceptor} from "./services/target.interceptor";
-import {REQUEST} from "@nguniversal/express-engine/tokens";
-import {Request} from "express";
-import {isPlatformServer} from "@angular/common";
-import {LANGUAGES} from "../constant";
-import {TranslateComponent} from "./components/translate.component";
-import {SalathielTitleStrategy} from "./services/salathiel.title-strategy";
-import {ReactiveFormsModule} from "@angular/forms";
+
+import {HeaderComponent} from "./header.component";
+import {FooterComponent} from "./footer.component";
+import {MainComponent} from './main.component';
+import {NavComponent} from "./nav.component";
 
 @NgModule({
   declarations: [
@@ -67,8 +51,6 @@ import {ReactiveFormsModule} from "@angular/forms";
     ReactiveFormsModule,
   ],
   providers: [
-    {provide: LANGUAGE_TAG, useFactory: () => inject(AppModule).resolve(LANGUAGE_TAG)},
-    {provide: IS_HOME, useFactory: () => inject(AppModule).resolve(IS_HOME)},
     {provide: HTTP_INTERCEPTORS, multi: true, useClass: TargetInterceptor},
     {provide: TitleStrategy, useClass: SalathielTitleStrategy},
     provideClientHydration(),
@@ -80,38 +62,4 @@ import {ReactiveFormsModule} from "@angular/forms";
   ]
 })
 export class AppModule {
-  readonly #isHome: WritableSignal<boolean>;
-  readonly #languageTag: WritableSignal<string>;
-
-  constructor(router: Router,
-              activatedRoute: ActivatedRoute,
-              @Inject(PLATFORM_ID) private readonly platformId: object,
-              @Inject(REQUEST) @Optional() private readonly request: Request) {
-    const navigationEnd = toSignal(
-        router.events.pipe(filter(_ => _ instanceof NavigationEnd))) as Signal<NavigationEnd>;
-    effect(() => {
-      this.#isHome.set(HomeComponent === activatedRoute.children[0]?.component);
-      this.#languageTag.set(this.#resolveLanguageTag());
-      navigationEnd();
-    }, {allowSignalWrites: true});
-    this.#isHome = signal(HomeComponent === activatedRoute.children[0]?.component);
-    this.#languageTag = signal(this.#resolveLanguageTag());
-  }
-
-  resolve<T>(token: InjectionToken<T>): T | void {
-    switch (token) {
-      case IS_HOME:
-        return this.#isHome as any;
-      case LANGUAGE_TAG:
-        return this.#languageTag as any;
-    }
-  }
-
-  #resolveLanguageTag(): string {
-    if (isPlatformServer(this.platformId)) {
-      return LANGUAGES.find(({tag}) => this.request.url.startsWith(`/${tag}`))?.tag ?? 'en-GB';
-    } else {
-      return LANGUAGES.find(({tag}) => location.pathname.startsWith(`/${tag}`))?.tag ?? 'en-GB';
-    }
-  }
 }
