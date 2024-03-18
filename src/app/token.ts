3e52ad78ba95136df6b43e04d945c85714249926
Salathiel <salathiel@genese.name>
2024-03-18 07:38:52 +0100

    fix: typo

--- src/app/token.ts
+++ src/app/token.ts
@@ -1,4 +1,4 @@
 import {InjectionToken, Signal} from "@angular/core";
 
 export const IS_HOME = Symbol('IS_HOME') as unknown as InjectionToken<Signal<boolean>>;
-export const LANGUAGE_TAG = Symbol('LOCALE') as unknown as InjectionToken<Signal<string>>;
+export const LANGUAGE_TAG = Symbol('LANGUAGE_TAG') as unknown as InjectionToken<Signal<string>>;
